46e411f9f734fd20c06ed183ccf5d0d5cb7ce5f2
Tyler Long <tyler4long@gmail.com>
2021-12-13 17:27:31 -0800

    Try OAuth

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('Hello world!');
+import RingCentral from '@rc-ex/core';
+import AuthorizeUriExtension from '@rc-ex/authorize-uri';
+
+const redirectUri = window.location.origin + window.location.pathname;
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+const authorizeUriExtension = new AuthorizeUriExtension();
+rc.installExtension(authorizeUriExtension);
+const urlSearchParams = new URLSearchParams(
+  new URL(window.location.href).search
+);
+
+const code = urlSearchParams.get('code');
+if (code === null) {
+  // login
+  const loginUrl = authorizeUriExtension.buildUri({redirect_uri: redirectUri});
+  const link = document.createElement('a');
+  link.href = loginUrl;
+  link.innerText = 'Login';
+  document.body.appendChild(link);
+} else {
+  // exchange code for token
+  (async () => {
+    const tokenInfo = await rc.authorize({code, redirect_uri: redirectUri});
+    console.log(tokenInfo.access_token);
+  })();
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,13 @@
+/* eslint-disable node/no-unpublished-require */
 /* eslint-disable node/no-unpublished-import */
-import {Configuration} from 'webpack';
+import {Configuration, DefinePlugin} from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
+import dotenv from 'dotenv-override-true';
 
 const config: Configuration = {
   mode: 'development',
+  devtool: 'source-map',
   entry: {
     index: './src/index.ts',
   },
@@ -15,7 +18,17 @@ const config: Configuration = {
     new HtmlWebpackPlugin({
       title: 'oauth-permission-issue-troubleshooting',
     }),
+    new DefinePlugin({
+      'process.env': JSON.stringify(dotenv.config().parsed),
+    }),
   ],
+  resolve: {
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      buffer: require.resolve('buffer/'),
+      stream: require.resolve('stream-browserify'),
+    },
+  },
 };
 
 export default config;
